fb98d105533d4a9f16f35ee37760e04cd98d3c9b
masum691 <masumhu691@gmail.com>
2021-10-29 01:37:03 +0600

    header

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
+    // const { user, logOut } = useAuth();
+    // console.log(user)
+
     return (
         <div>
-            
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand nav-bar fs-3" href="/home">World <span className="text-primary">Travel</span></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
+                            <li class="nav-item">
+                                <NavLink className="link nav-link"
+                                    to="/home"
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li class="nav-item">
+                                <NavLink className="link nav-link"
+                                    to="/services"
+                                >
+                                    Services
+                                </NavLink>
+                            </li>
+                            <li class="nav-item">
+                                <NavLink className="link nav-link"
+                                    to="/about"
+                                >
+                                    About Us
+                                </NavLink>
+                            </li>
+                            <li class="nav-item">
+                                <NavLink className="link nav-link"
+                                    to="/contact"
+                                >
+                                    Contact
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
