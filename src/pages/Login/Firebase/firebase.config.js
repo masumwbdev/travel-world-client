79e8f513afbb0761c011494e384776f4ba180cf4
masum691 <masumhu691@gmail.com>
2021-10-29 08:49:24 +0600

    firebase config and init

---
+++ src/pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCAoe_pm568FTs842PgmthChwjYJTh3Kwc",
+    authDomain: "world-travel-inc.firebaseapp.com",
+    projectId: "world-travel-inc",
+    storageBucket: "world-travel-inc.appspot.com",
+    messagingSenderId: "103391273187",
+    appId: "1:103391273187:web:b952d8238895ab78df65d6"
+};
+export default firebaseConfig;
