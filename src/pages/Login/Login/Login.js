d587fe6fe68ca39145a6bed2fe149dfc862adfd8
masum691 <masumhu691@gmail.com>
2021-10-29 22:01:22 +0600

    login button

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHistory, useLocation } from 'react-router';
 import useFirebase from '../../hooks/useFirebase';
+import './Login.css'
 
 const Login = () => {
     const { handleGoogleSignIn, setError } = useFirebase();
@@ -15,8 +16,9 @@ const Login = () => {
             })
     }
     return (
-        <div>
-            <button onClick={signInUsingGoogle}>Google Sign in</button>
+        <div className="google-button">
+            <h1 className="mb-4 fs-3">Sign in Using <span className="text-primary">Google</span></h1>
+            <button className="google-sign-in" onClick={signInUsingGoogle}><i class="fab fa-google fs-1 me-2 google-sign"></i> Google Sign in</button>
         </div>
     );
 };
